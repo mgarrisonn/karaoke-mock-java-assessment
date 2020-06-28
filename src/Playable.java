4ffa99c446e1f88f1c51e57e8e4447ab7e838000
michael garrison <mgarrison.sa@gmail.com>
2020-06-27 20:16:13 -0500

    added method for song and album/implemented interface on player

--- src/Playable.java
+++ src/Playable.java
@@ -1,2 +1,6 @@
 public interface Playable {
+
+    void play(Album a);
+    void play(Song s);
+
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,5 @@
-public class Player {
+public abstract class Player implements Playable {
+
+    public final int WORD_CADENCE = 600;
+    public final int INTRO_PAUSE = 7000;
 }
